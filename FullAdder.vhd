20f67f9b646085527616d9c76e5c42206548c3af
Củ Chuối <60062619+nguyentuanqs@users.noreply.github.com>
2021-03-12 09:39:41 +0700

    Update FullAdder.vhd

--- FullAdder.vhd
+++ FullAdder.vhd
@@ -1,2 +1,17 @@
 library ieee;
-use std_
+use ieee.std_logic_arith.all;
+use ieee.std_logic_unsigned.all;
+entity full_adder is 
+  port
+    (
+      a, b, cin: in std_logic;
+      s, cout: out std_logic
+    );
+  end full_adder;
+  -----
+  architecture dataflow of full_adder is 
+    begin 
+      s <= a xor b xor cin;
+      cout <= (a and b) or (cin and (a or b));
+    end dataflow;
+  
